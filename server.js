971f5867a4e804b98fb7dd2cb71ae016521316aa
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-08-14 11:11:42 -0400

    added routes for homepage & notes

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const { response } = require('express');
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const db =  require('./db/db.json');
 
@@ -9,6 +10,14 @@ app.get('/db', (req, res) => {
     res.json(db);
 });
 
+app.get ('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
+
+app.get ('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+})
+
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`)
+    console.log(`API server now on port ${PORT}!`);
 })
